4770fac276d715c310a8a5d5b4980b59726fb4d5
bootcamp-seekerscloud <seekerscloudbootcamp@gmail.com>
2022-10-15 12:19:29 -0700

    init

--- src/app/modules/customer/customer-routing.module.ts
+++ src/app/modules/customer/customer-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './customer.component';
+import { DeleteCustomerComponent } from './module/delete-customer/delete-customer.component';
+import { NewCustomerComponent } from './module/new-customer/new-customer.component';
 
-const routes: Routes = [{ path: '', component: CustomerComponent }];
+const routes: Routes = [
+  { path: '', component: CustomerComponent, children:[
+    {path:'new-customer', component:NewCustomerComponent},
+    {path:'delete-customer', component:DeleteCustomerComponent},
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
+import { NewCustomerComponent } from './module/new-customer/new-customer.component';
+import { DeleteCustomerComponent } from './module/delete-customer/delete-customer.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    NewCustomerComponent,
+    DeleteCustomerComponent
   ],
   imports: [
     CommonModule,
