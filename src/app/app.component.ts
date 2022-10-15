fdebc378cd5049dc7ec4f03ca7ae844d96276097
bootcamp-seekerscloud <seekerscloudbootcamp@gmail.com>
2022-10-15 09:30:30 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app1';
+  
+userForm= new FormGroup({
+  username: new FormControl(null),
+  password: new FormControl(null)
+});
+
+public submitData(){
+  console.log(this.userForm);
+}
+
+
 }
